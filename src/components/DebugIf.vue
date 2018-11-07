c9d3cfcd5191a44cdc6ce8352640152c7cb4e2d1
Ben <bmgrimes@gmail.com>
2018-11-07 13:58:40 -0600

    Init

---
+++ src/components/DebugIf.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="debugIf">
+    <div v-if="override ? override || condition : condition">
+      <slot></slot>
+    </div>
+    <button v-if="DEBUG_MODE" class="debugBtn" @click="override = !override">Turn override {{override ? 'OFF' : 'ON'}}</button>
+  </div>
+</template>
+<script>
+export default {
+  name: 'DebugIf',
+  props: {
+    condition: Boolean
+  },
+  data () {
+    return {
+      DEBUG_MODE: true,
+      override: false
+    }
+  }
+}
+</script>
