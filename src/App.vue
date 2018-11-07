60b6112df4e67dc3de26ffba038194cf1d772d3b
Ben <bmgrimes@gmail.com>
2018-11-07 14:07:43 -0600

    Remove hello world junk

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    Condition: {{ival}}
+    <DebugIf :condition="ival">
+      Conditional debug content
+    </DebugIf>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import DebugIf from "./components/DebugIf"
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    DebugIf
+  },
+  data () {
+    return {
+      ival: false
+    }
   }
 }
 </script>
